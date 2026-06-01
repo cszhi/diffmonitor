f14835722a743c3f7962563ea9a00af8ac342f85
cszhi <cszhi@live.com>
2026-06-01 09:56:59 +0800

    Improve client script stability

--- client/diff_m.sh
+++ client/diff_m.sh
@@ -8,39 +8,43 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 DIR=$(cd "$(dirname "$0")"; pwd)
-LOG=$DIR/log
+LOG="$DIR/log"
+LOCK_FILE="$DIR/diff_m.lock"
 API_TOKEN=${API_TOKEN:-changeme}
-CURL="curl -s --connect-timeout 30 -H X-API-Token:$API_TOKEN"
+CURL="curl -s --connect-timeout 10 --max-time 60 -H X-API-Token:$API_TOKEN"
 [ -n "$1" ] && SERVER=$1 || { echo "Please enter server address!!"; exit 1; }
 API="http://$SERVER/api"
 HOSTNAME=${2:-$HOSTNAME}
 IP=${3:-127.0.0.1}
 
-[ -d $DIR/tmp ] || mkdir -p $DIR/tmp
+exec 9>"$LOCK_FILE"
+flock -n 9 || { echo "diffmonitor is already running"; exit 0; }
+
+[ -d "$DIR/tmp" ] || mkdir -p "$DIR/tmp"
 #log
 log()
 {
-	echo "`date '+%F %T'`   $1" >>$LOG
+	echo "`date '+%F %T'`   $1" >>"$LOG"
 }
 
-[ -f $DIR/list.conf ] || { log "error!! list.conf does not exist!!"; exit 1; }
+[ -f "$DIR/list.conf" ] || { log "error!! list.conf does not exist!!"; exit 1; }
 
 #ops
 ops() 
 {
-	if [ $2 == "file" ];then
-		[ -f $3 ] || { log "error!! file $3 does not exist!!"; continue; }
-		md5=$(md5sum $3 |awk '{print $1}')
+	if [ "$2" == "file" ];then
+		[ -f "$3" ] || { log "error!! file $3 does not exist!!"; continue; }
+		md5=$(md5sum "$3" |awk '{print $1}')
 		content="$3"
-	elif [ $2 == "shell" ];then
-		[ -f $DIR/shell/$3 ] || { log "error!! shell $3 does not exist!!"; continue; }
-		sh $DIR/shell/$3 >$DIR/shell/z_${1} 2>>$LOG
+	elif [ "$2" == "shell" ];then
+		[ -f "$DIR/shell/$3" ] || { log "error!! shell $3 does not exist!!"; continue; }
+		sh "$DIR/shell/$3" >"$DIR/shell/z_${1}" 2>>"$LOG"
 		[ $? -eq 0 ] || { echo "shell $3 error, please check!"; continue; }
-		md5=$(md5sum $DIR/shell/z_${1} |awk '{print $1}')
+		md5=$(md5sum "$DIR/shell/z_${1}" |awk '{print $1}')
 		content="$DIR/shell/z_${1}"
 	fi
 
-	oldmd5=`$CURL $API/md5 -d hostname="$HOSTNAME" -d type="$1" -d md5="$md5"`
+	oldmd5=`$CURL "$API/md5" -d hostname="$HOSTNAME" -d type="$1" -d md5="$md5"`
 	if [ $? -eq 0 ];then
 		log "oldmd5=$oldmd5 newmd5=$md5 $1"
 	else
@@ -52,29 +56,29 @@ ops()
 		continue
 	elif [ "$oldmd5" == "null" ];then
 		if [ "$4" == "hashonly" ];then
-			$CURL $API/create -d hostname="$HOSTNAME" -d ip="$IP" -d type="$1" -d md5="$md5" --data-urlencode content="[hashonly] content hidden"
+			$CURL "$API/create" -d hostname="$HOSTNAME" -d ip="$IP" -d type="$1" -d md5="$md5" --data-urlencode content="[hashonly] content hidden"
 		else
-			$CURL $API/create -d hostname="$HOSTNAME" -d ip="$IP" -d type="$1" -d md5="$md5" --data-urlencode content="`cat $content`"
+			$CURL "$API/create" -d hostname="$HOSTNAME" -d ip="$IP" -d type="$1" -d md5="$md5" --data-urlencode content="`cat "$content"`"
 		fi
 		[ $? -eq 0 ] && log "create $HOSTNAME $1 success" || log "create $HOSTNAME $1 failed"
 		continue
 	elif [ "$oldmd5" != "$md5" ];then
 		if [ "$4" == "hashonly" ];then
-			$CURL $API/update -d hostname="$HOSTNAME" -d type="$1" -d newmd5="$md5" --data-urlencode newcontent="[hashonly] content hidden" --data-urlencode diff="[hashonly] content changed"
+			$CURL "$API/update" -d hostname="$HOSTNAME" -d type="$1" -d newmd5="$md5" --data-urlencode newcontent="[hashonly] content hidden" --data-urlencode diff="[hashonly] content changed"
 		else
-			$CURL $API/content -d hostname="$HOSTNAME" -d type="$1" >$DIR/tmp/${1}.tmp
-			sed -i -e '$a\' $DIR/tmp/${1}.tmp
-			DIFFCONTENT=$(diff $DIR/tmp/${1}.tmp $content)
-			$CURL $API/update -d hostname="$HOSTNAME" -d type="$1" -d newmd5="$md5" --data-urlencode newcontent="`cat $content`" --data-urlencode diff="$DIFFCONTENT";
+			$CURL "$API/content" -d hostname="$HOSTNAME" -d type="$1" >"$DIR/tmp/${1}.tmp"
+			sed -i -e '$a\' "$DIR/tmp/${1}.tmp"
+			DIFFCONTENT=$(diff "$DIR/tmp/${1}.tmp" "$content")
+			$CURL "$API/update" -d hostname="$HOSTNAME" -d type="$1" -d newmd5="$md5" --data-urlencode newcontent="`cat "$content"`" --data-urlencode diff="$DIFFCONTENT";
 		fi
 		[ $? -eq 0 ] && log "update $HOSTNAME $1 success" || log "update $HOSTNAME $1 failed"
 	fi
 }
 
-cat $DIR/list.conf |grep -v "^#" |while read name type action mode;
+grep -v "^#" "$DIR/list.conf" |while read name type action mode;
 do
 	sleep 1
-	ops $name $type $action $mode
+	ops "$name" "$type" "$action" "$mode"
 done
 
-echo "" >>$LOG
+echo "" >>"$LOG"
